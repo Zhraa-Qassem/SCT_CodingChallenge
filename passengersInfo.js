d2c0220f2f5f528b52dedd724e7b24da68a3d61b
zhraa <zhraa2005spn@gmail.com>
2023-03-04 22:12:16 +0300

    'commit'

--- passengersInfo.js
+++ passengersInfo.js
@@ -1,29 +1,65 @@
 
-function checkPassengersCount(){
+function checkPassengersCount() {
+    let NoOfTravellers = 0;
+    for (let i = 0; i < passengerInfo.length; i++) {
+        NoOfTravellers = NoOfTravellers + 1;
+    }
 
     return NoOfTravellers;
 }
 
-function checkPassengersOfSameAge(){
+
+
+
+
+function checkPassengersOfSameAge() {
+    let ids = {};
+    let noOfPassngersWithSimilarAge = [];
     
-    return noOfPassngersWithSimilarAge; 
+    passengerInfo.forEach((val)=> {
+      if (ids[val.passengerAge]) {
+        noOfPassngersWithSimilarAge.push(val)
+      } else {
+        ids[val.passengerAge] = true;
+      }
+    })
+   return noOfPassngersWithSimilarAge;
 }
-    
+
+function addNewPassenger(name,age,passportNo){
+passengerInfo.push({
+    passengerName: name, passengerAge: age , passengerPassportNo:passportNo
+})
+
+}
+
 const passengerInfo = [{
-        passengerName: "Noor",
-        passengerAge: 24,
-        passengerPassportNo: "A123456"
-    }, 
-    {
-    passengerName:"Zahraa",
-    passengerAge:23,
-     passengerPassportNo: "A67899",
- },
- {
+    passengerName: "Noor",
+    passengerAge: 24,
+    passengerPassportNo: "A123456"
+},
+{
+    passengerName: "Zahraa",
+    passengerAge: 23,
+    passengerPassportNo: "A67899",
+},
+{
     passengerName: "Ahmed",
     passengerAge: 24,
     passengerPassportNo: "A14343"
-}, ]
-    
-    console.log("number of passengers: " , checkPassengersCount(passengerInfo))
-    console.log("number of passenger who have same age : " , checkPassengersOfSameAge(passengerInfo))
+},]
+
+
+var passengerCount = checkPassengersCount()
+var passengersOfSameAge = checkPassengersOfSameAge()
+//console.log(passengerCount)
+//console.log(passengersOfSameAge)
+document.write('number of passengers:',' '+ passengerCount)
+//document.write("\n"+"number of passenger who have same age : ",' '+ passengersOfSameAge)
+
+console.log("number of passengers: ", checkPassengersCount(passengerInfo))
+console.log("\r\n"+"number of passenger who have same age : ", checkPassengersOfSameAge(passengerInfo))
+
+//extra cridet question
+addNewPassenger('zhraa',20,8765);
+console.log(passengerInfo)
